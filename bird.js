95007e14998b379cf8c558d30e42e056a3ad0b80
IronCladCode <michaelmazzella12@gmail.com>
2020-07-24 02:21:27 -0400

    implementing a moveable bird(black square)

--- bird.js
+++ bird.js
@@ -1,5 +1,23 @@
 class Bird {
   constructor() {
+    this.xLocation = 70;
+    this.yLocation = 0;
+    this.velocity = 0;
+    this.orientation = 90;
+    // this.img = new Image();
+    // this.img.src = 'bird.png';
+  }
 
+  show(){
+    ctx.fillRect(this.xLocation - 5, this.yLocation - 5, 10, 10);
+  }
+
+  update(){
+    this.velocity += .5;
+    this.yLocation += this.velocity;
+  }
+
+  flap(){
+    this.velocity = -8;
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,9 +4,21 @@ const ctx = canvas.getContext("2d");
 const background = new Background();
 const bird = new Bird();
 
+document.addEventListener("keydown", (keycode) => {
+  if(keycode.code == 'Space'){
+    bird.flap();
+  }
+});
+
 const draw = () => {
+  ctx.rect(0,0,canvas.width,canvas.height);
+  ctx.fill();
+
   background.scroll();
   background.show();
+
+  bird.update();
+  bird.show();
 }
 
 const interval = setInterval(draw, 17);
